43a4152d3500a90062367a74510e284013a18d38
yellowsea <yunhai0644@163.com>
2022-10-08 15:32:30 +0800

    fix: run eslint:fix

--- src/main.js
+++ src/main.js
@@ -1,14 +1,13 @@
 import Vue from 'vue'
-import { createPinia, PiniaVuePlugin } from 'pinia'
+import { PiniaVuePlugin, createPinia } from 'pinia'
 
 import App from './App.vue'
 import router from './router'
-
 
 Vue.use(PiniaVuePlugin)
 
 new Vue({
   router,
   pinia: createPinia(),
-  render: (h) => h(App)
+  render: h => h(App),
 }).$mount('#app')
--- src/router/index.js
+++ src/router/index.js
@@ -1,14 +1,13 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 
-
 Vue.use(VueRouter)
 
 const router = new VueRouter({
   mode: 'history',
   base: import.meta.env.BASE_URL,
   routes: [
-  ]
+  ],
 })
 
 export default router
